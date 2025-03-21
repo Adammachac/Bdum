1dd31d99dfa69506d38313d1a1947fc26d2face6
Adammachac <148778367+Adammachac@users.noreply.github.com>
2025-03-21 16:47:08 +0100

    Add files via upload

---
+++ jssoubory/fadein.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const fadeInElements = document.querySelectorAll('.fade-in');
+
+    const observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('visible');
+                observer.unobserve(entry.target);
+            }
+        });
+    }, { threshold: 0.1 });
+
+    fadeInElements.forEach(element => {
+        observer.observe(element);
+    });
+});
